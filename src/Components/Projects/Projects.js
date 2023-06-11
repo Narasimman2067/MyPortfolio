23574a3bfe52f09a54d86069c8fe1c15a730f023
Narasimman s <118879166+Narasimman2067@users.noreply.github.com>
2023-06-11 20:46:24 +0530

    Update Projects.js

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,21 +10,21 @@ export const Projects = () => {
     {
       title: "Gmail Clone App",
 
-      description: "Design & Developed by Narasimman",
+      description: "Basic features of Gmail clone app ",
       img:
         "https://cdn.vox-cdn.com/thumbor/ZhpUtEN49QhrTeiNWKZGIx-ji20=/63x0:737x449/1310x873/cdn.vox-cdn.com/uploads/chorus_image/image/67587449/newgmaillogo.0.gif",
       link: "https://gmail-clone-app2023.netlify.app/",
     },
     {
       title: "Youtube Clone App",
-      description: "using with javascript basic functions on fetch api from google cloud api data ",
+      description: "using javascript basic functions on fetch api from google cloud api data ",
       img:
         "https://cdn.vox-cdn.com/thumbor/CQXisvpqlzIPjk4ETDokVBaQuqg=/0x0:1680x1050/1575x1050/filters:focal(840x525:841x526):no_upscale()/cdn0.vox-cdn.com/uploads/chorus_asset/file/9130449/YTLogo_old_new_animation.gif",
       link: "https://snazzy-froyo-58c8fd.netlify.app/",
     },
     {
       title: "BookStore Crud App",
-      description: "Using MERN Bookstore Basic Crud Function app ",
+      description: "Using Crud Operation made a Bookstore app on Mern Stack development.",
       img:
         "https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fa26cc294-2f30-4aae-974e-b0c9f0b48590_672x672.gif",
       link: "https://mern-frontend-bookstore.vercel.app/",
